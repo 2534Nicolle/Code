c911deee17301cde35ed92a2afdc3d41a4711194
Nicks <nicollegoncalves1331@gmail.com>
2024-03-06 10:55:39 -0300

    Primeira versão do Ola mundo

---
+++ Garro/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Nicolle
+ *
+ */
+module Garro {
+}
---
+++ LPA/src/lpa/Garro.java
@@ -0,0 +1,10 @@
+package lpa;
+
+public class Garro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
